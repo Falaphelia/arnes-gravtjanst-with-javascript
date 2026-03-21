95eeed0c8adf6443ae5f94458765f18021680ddf
Falaphelia <simonkarlsson797@gmail.com>
2026-03-21 12:09:50 +0100

    fix more ;

--- scripts/image_switcher.js
+++ scripts/image_switcher.js
@@ -1,17 +1,17 @@
 const images = ["../img/machine1.jpg", "../img/machine2.jpg", "../img/machine3.jpg", "../img/machine4.jpg",
-    "../img/machine5.jpg", "../img/machine6.jpg", "../img/machine7.jpg", "../img/machine8.jpg"]
+    "../img/machine5.jpg", "../img/machine6.jpg", "../img/machine7.jpg", "../img/machine8.jpg"];
 
-const counter = document.getElementById("image_number")
+const counter = document.getElementById("image_number");
 const displayImg = document.getElementById("main-gallery-img");
 const nextBtn = document.getElementById("next-btn");
 const prevBtn = document.getElementById("prev-btn");
 let currentImageIndex = 0;
 
-counter.innerHTML = "Bild: " + (currentImageIndex+1) + "/" + images.length
+counter.innerHTML = "Bild: " + (currentImageIndex+1) + "/" + images.length;
 
 function updateImage() {
     displayImg.src = images[currentImageIndex];
-    counter.innerHTML = "Bild: " + (currentImageIndex+1) + "/" + images.length /*Also updates text since we only
+    counter.innerHTML = "Bild: " + (currentImageIndex+1) + "/" + images.length; /*Also updates text since we only
     ever need to update it when updating picture*/
 }
 
@@ -21,7 +21,7 @@ function next_image() {
         currentImageIndex = 0;
     } else {currentImageIndex++}
 
-    updateImage()
+    updateImage();
 }
 
 function prev_image() {
